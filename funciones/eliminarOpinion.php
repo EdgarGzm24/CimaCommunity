98a22845b795a362bb74a203913da80639ae279d
UziLopez <106778312+UziLopez@users.noreply.github.com>
2022-12-06 00:00:01 -0800

    i

--- funciones/eliminarOpinion.php
+++ funciones/eliminarOpinion.php
@@ -1,9 +1,9 @@
 <?php
    require_once  'conexion.php';
     $publi = $_GET['id'];
-    $sql = "DELETE FROM opiniones  WHERE  publi ='idopiniones'";
+    $sql = "DELETE FROM opiniones  WHERE   idopiniones = '$publi'";
     $query = mysqli_query($conexion,$sql);
     if($query){
-        Header("Location: publicacionOpinion.php");
+        Header("Location: ../publicacionOpinion.php");
     }
 ?>
