00cb4ab55d9e249fd222dc52b2870c633b75c5f1
Edgar Arellano Guzman <69610495+EdgarGzm24@users.noreply.github.com>
2022-12-06 20:50:34 -0800

    1

--- publicacionComentarios.php
+++ publicacionComentarios.php
@@ -389,32 +389,12 @@
     <script>
     // Modals
     $(document).ready(function(){
-
-
-        $("#messagesmodal").hover(function(){
-            $(".modal-comments").toggle();
-        });
-        $(".modal-comments").hover(function(){
-            $(".modal-comments").toggle();
-        });
-
-
-
-        $("#friendsmodal").hover(function(){
-            $(".modal-friends").toggle();
-        });
-        $(".modal-friends").hover(function(){
-            $(".modal-friends").toggle();
-        });
-
-
         $("#profilemodal").hover(function(){
             $(".modal-profile").toggle();
         });
         $(".modal-profile").hover(function(){
             $(".modal-profile").toggle();
         });
-
 
         $("#navicon").click(function(){
             $(".mobilemenu").fadeIn();
