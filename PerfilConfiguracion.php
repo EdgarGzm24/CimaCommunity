dfe0849853435cd128568cea1f15b2ade05d83d4
UziLopez <106778312+UziLopez@users.noreply.github.com>
2022-12-06 23:08:10 -0800

    ii

--- PerfilConfiguracion.php
+++ PerfilConfiguracion.php
@@ -9,6 +9,8 @@
     <!--===============================================================================================-->	
     <link rel="stylesheet" type="text/css" href="css/estilosInicio.css">
     <!--===============================================================================================-->	
+    <link rel="stylesheet" type="text/css" href="css/estilosPerfil.css">
+    <!--===============================================================================================-->	
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <!--===============================================================================================-->	
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
@@ -45,7 +47,7 @@
     </div>
 
     <div class="all">
-        <div class="">
+        <div class="fondo">
 
         </div>
 
