050681ef427431a6f3027f9e173bd2c545d0ebdc
UziLopez <106778312+UziLopez@users.noreply.github.com>
2022-12-06 23:59:19 -0800

    ii

--- Inicio.php
+++ Inicio.php
@@ -54,7 +54,7 @@
         <div class="navbar_search">
             <form method="POST" action="funciones/validaComunidades.php">
                 <input type="text" name="buscarComunidad" id="buscarComunidad" placeholder="Busca tu comunidad.." autocomplete="off"/>
-                <button type="submit"> <a href="#" class="fa fa-search"></a></button>
+                <button type="submit"> <a href="#" class="fa fa-search icon-search"></a></button>
             </form>            
             <div id="show-list"></div>
         </div>
