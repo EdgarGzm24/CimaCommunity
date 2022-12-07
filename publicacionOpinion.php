9be3d16c9671f58d02b59f220126f406807139ee
UziLopez <106778312+UziLopez@users.noreply.github.com>
2022-12-06 19:18:40 -0800

    ii

--- publicacionOpinion.php
+++ publicacionOpinion.php
@@ -335,6 +335,18 @@
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!--===============================================================================================-->
+	<script src="js/jquery-3.6.1.js"></script>
+	<!--===============================================================================================-->
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<!--===============================================================================================-->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+	<!--===============================================================================================-->
+	<script src="https://kit.fontawesome.com/f75ca2de84.js" crossorigin="anonymous"></script>
+	<!--===============================================================================================-->
+	<script src="js/funcionesAjax.js"></script> 
+	<script src="js/jsimgPrevia.js"></script>
+
     <script>
     // Modals
     $(document).ready(function(){
